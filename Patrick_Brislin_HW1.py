052713091952d7e3c2a55550b9ae33d8250993c4
pbrislin1 <Patrickbrislin40@gmail.com>
2025-02-22 11:58:16 -0500

    Patrick_Brislin_Ride_Sharing.py

--- Patrick_Brislin_HW1.py
+++ Patrick_Brislin_HW1.py
@@ -60,49 +60,49 @@ class RideSharingApp:
     
     
 
-#For the remaining code (after this line), no modification is required
-print('---------------------Object Creation------------------')
-location1 = Location(2,1)
-location2 = Location(-4,1)
-car1 = Car('car1', location1, 0.61)
-car2 = Car('car2', location2, 0.50)
-print('Car object 1 created:',car1)
-print('Car object 2 created:', car2)
-
-location3 = Location(-2,3)
-location4 = Location(0,0)
-passenger1 = Passenger('passenger1', location3)
-passenger2 = Passenger('passenger2', location4)
-print('Passenger object 1 created:', passenger1)
-print('Passenger object 2 created:', passenger2)
-
-mobileApp = RideSharingApp()
-mobileApp.add_car(car1)
-mobileApp.add_car(car2)
-mobileApp.add_passenger(passenger1)
-mobileApp.add_passenger(passenger2)
-
-print('-----------------------Scenario 1---------------------')
-mobileApp.find_cheapest_car()
-mobileApp.find_nearest_car(passenger1)
-mobileApp.find_nearest_car(passenger2)
-
-print('-----------------------Scenario 2---------------------')
-car1.move_to(0,-5)
-passenger1.move_to(0,3)
-print('car1\'s location has been updated:',car1)
-print('passenger1\'s location has been updated:', passenger1)
-
-mobileApp.find_cheapest_car()
-mobileApp.find_nearest_car(passenger1)
-mobileApp.find_nearest_car(passenger2)
-
-print('-----------------------Scenario 3---------------------')
-car3= Car('car3', Location(0,2), 0.3)
-mobileApp.add_car(car3)
-print('New car added:',car3)
-
-mobileApp.find_cheapest_car()
-mobileApp.find_nearest_car(passenger1)
-mobileApp.find_nearest_car(passenger2)
+if __name__ == "__main__":
+    print('---------------------Object Creation------------------')
+    location1 = Location(2,1)
+    location2 = Location(-4,1)
+    car1 = Car('car1', location1, 0.61)
+    car2 = Car('car2', location2, 0.50)
+    print('Car object 1 created:',car1)
+    print('Car object 2 created:', car2)
+    
+    location3 = Location(-2,3)
+    location4 = Location(0,0)
+    passenger1 = Passenger('passenger1', location3)
+    passenger2 = Passenger('passenger2', location4)
+    print('Passenger object 1 created:', passenger1)
+    print('Passenger object 2 created:', passenger2)
+    
+    mobileApp = RideSharingApp()
+    mobileApp.add_car(car1)
+    mobileApp.add_car(car2)
+    mobileApp.add_passenger(passenger1)
+    mobileApp.add_passenger(passenger2)
+    
+    print('-----------------------Scenario 1---------------------')
+    mobileApp.find_cheapest_car()
+    mobileApp.find_nearest_car(passenger1)
+    mobileApp.find_nearest_car(passenger2)
+    
+    print('-----------------------Scenario 2---------------------')
+    car1.move_to(0,-5)
+    passenger1.move_to(0,3)
+    print('car1\'s location has been updated:',car1)
+    print('passenger1\'s location has been updated:', passenger1)
+    
+    mobileApp.find_cheapest_car()
+    mobileApp.find_nearest_car(passenger1)
+    mobileApp.find_nearest_car(passenger2)
+    
+    print('-----------------------Scenario 3---------------------')
+    car3= Car('car3', Location(0,2), 0.3)
+    mobileApp.add_car(car3)
+    print('New car added:',car3)
+    
+    mobileApp.find_cheapest_car()
+    mobileApp.find_nearest_car(passenger1)
+    mobileApp.find_nearest_car(passenger2)
 
